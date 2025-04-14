69c3424f90f9000a81c4a8b208eee3189a6dcfc8
Naeel <tazet@narod.ru>
2025-04-14 20:50:45 +0400

    md ok

--- 2_Exodus/funcer.tf
+++ 2_Exodus/funcer.tf
@@ -1,9 +1,13 @@
+// Сервисный аккаунт — аккаунт, от имени которого программы могут управлять ресурсами в Yandex Cloud.
+// https://yandex.cloud/ru/docs/iam/concepts/users/service-accounts?from=int-console-help-center-or-nav
 # create SERVICE ACCOUNT
 resource "yandex_iam_service_account" "goy-sa" {
   name        = "goy-service-account"
   folder_id   = var.folder_id
   description = "service account for function"
 }
+// Статический ключ доступа необходим для аутентификации сервисного аккаунта в AWS-совместимых API.
+// https://yandex.cloud/ru/docs/iam/concepts/authorization/access-key
 # create  static ACCESS KEY
 resource "yandex_iam_service_account_static_access_key" "goy-static-key" {
   service_account_id = yandex_iam_service_account.goy-sa.id
@@ -12,7 +16,7 @@ resource "yandex_iam_service_account_static_access_key" "goy-static-key" {
 # create role for service account
 resource "yandex_resourcemanager_folder_iam_member" "goy-role" {
   for_each = toset([
-    "functions.functionInvoker",
+  //  "functions.functionInvoker",
   ])
   role      = each.value
   folder_id = var.folder_id
@@ -26,18 +30,18 @@ resource "yandex_function" "goy" {
   entrypoint         = "Code.Sender"
   memory             = "256"
   execution_timeout  = "110"
-  service_account_id = yandex_iam_service_account.goy-sa.id
-  environment = {
-    AWS_ACCESS_KEY_ID     = yandex_iam_service_account_static_access_key.goy-static-key.access_key
-    AWS_SECRET_ACCESS_KEY = yandex_iam_service_account_static_access_key.goy-static-key.secret_key
-    AWS_DEFAULT_REGION    = var.compute-default-zone
-  }
+#  service_account_id = yandex_iam_service_account.goy-sa.id
+  # environment = {
+  #   AWS_ACCESS_KEY_ID     = yandex_iam_service_account_static_access_key.goy-static-key.access_key
+  #   AWS_SECRET_ACCESS_KEY = yandex_iam_service_account_static_access_key.goy-static-key.secret_key
+  #   AWS_DEFAULT_REGION    = var.compute-default-zone
+  # }
   // Хеш архива с кодом. Обновляется при внесении изменений в код. Если хеш неизменен - компилляция не запускается
   user_hash = data.archive_file.lambda.output_base64sha256 
   content { zip_filename = "goim.zip" }
 }
 
-# роль functions.functionInvoker - кто может вызывать функцию function_id :
+# роль functions.functionInvoker - КТО может вызывать функцию function_id :
 resource "yandex_function_iam_binding" "function-iamw" {
   function_id = yandex_function.goy.id
   role        = "functions.functionInvoker"
@@ -47,7 +51,7 @@ resource "yandex_function_iam_binding" "function-iamw" {
   ]
 }
 # output - вывод в консоль URL публичной функции
-output "write_func" {
+output "metrics_func" {
   value = "https://functions.yandexcloud.net/${yandex_function.goy.id}"
 }
 
